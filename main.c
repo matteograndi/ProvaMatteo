dc832f52a4ddf911f6663fc5a39b783a742c4fbc
matteo <matteo@matteo-MS-7253.(none)>
2013-03-04 11:51:30 +0100

    change main without for

--- main.c
+++ main.c
@@ -16,20 +16,20 @@ int main()
   printf("Commento scritto sul ramo secondario\n");
   printf("\n");
   printf("PREMI:\n1) per entrare nel primo programma\n2) per entrare nel secondo programma\n3) per entrare nel terzo programma\n0) per uscure\n\n");
-printf("Inserisci un numero: ");
- scanf("%d",&num);
-  while (num!=0){
-    if(num==1)
+//printf("Inserisci un numero: ");
+// scanf("%d",&num);
+ // while (num!=0){
+  //  if(num==1)
       a=primo();
-    else if(num==2)
+ //   else if(num==2)
       a=secondo();
-    else if(num==3)
+  //  else if(num==3)
       a=terzo();
-      else
-	printf("Premi un alto numero\n");
-   a=a+1;
-printf("Inserisci un numero: ");
- scanf("%d",&num);
-  }
+  //    else
+//	printf("Premi un alto numero\n");
+ //  a=a+1;
+//printf("Inserisci un numero: ");
+// scanf("%d",&num);
+//  }
  return(0);
 }
